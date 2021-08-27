4d4a152e63794600a8564defd92bd2f0be2fab0d
zzAlfa <yuricoutinhoc@gmail.com>
2021-08-27 19:33:31 -0300

    melhoria na criptografia

--- crip.py
+++ crip.py
@@ -1,21 +1,25 @@
-def cript(name, key1, key2):
+import random
+
+
+alfa = ['n', 'w', 'r', 'x', '7', 'o', '2', 'z', '6', 'v', 'f',
+        '8', 'k', 'a', 'm', 'q', '1', 'c', 'y', '4', 'd', 'j',
+        'b', 'u', '9', 's', '0', 'p', 'g', '3', 't', '5', 'i', 'e', 'l', 'h']
+
+
+def cript(name, key1, key2, rkey1, rkey2):
     newname = ''
     for i in range(0, len(name)):
         a = name[i]
-
-        alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         for c in range(0, 36):
             if a == alfa[c]:
                 a = 5 + c
-                a = (a ** key1 + key2 ** 2) / 230
-                b = (a * 3) - 1 / 4
+                a = (a ** key1 + key2 ** 2) * rkey1 / rkey2
+                b = (a * rkey2) - 1 / (a / 2)
                 a = int(a)
                 b = int(b)
                 b = str(b)
                 a = str(a)
-                a = a + alfa[c - 3] + b + alfa[c - 15]
+                a = a + alfa[c - rkey2] + b + alfa[c - rkey1]
 
         newname = newname + a
     return newname
@@ -26,13 +30,27 @@ def menu():
     print('Created by: zzAlfa')
     print('')
     nome = str(input('Digite aqui o que deseja criptografar: ')).lower().strip()
-    k1 = int(input('Digite um número para ser a chave [0 para padrão]: '))
+    k1 = int(input('Digite um número para ser o primeiro chave [0 para padrão]: '))
     k2 = int(input('Digite um número para ser o segundo valor da chave [0 para padrão]: '))
     if k1 == 0:
         k1 = 2
     if k2 == 0:
         k2 = 6
-    print(f' Criptografado fica: {cript(nome, k1, k2)} \nAs chaves são {k1}-{k2}')
+    esc = str(input('Deseja que as duas últimas chaves sejam aleatórias? [S/N] ')).lower()
+    while True:
+        if esc == 's':
+            ran1 = random.randint(1, len(alfa))
+            ran2 = random.randint(1, len(alfa))
+            break
+        elif esc == 'n':
+            ran1 = int(input('Digite um número para ser o terceiro valor da chave [0 para padrão]: '))
+            ran2 = int(input('Digite um número para ser o quarto valor da chave [0 para padrão]: '))
+            break
+        else:
+            print('Digite novamente, tente digitar apenas s ou n')
+            esc = str(input('Deseja que as duas últimas chaves sejam aleatórias? [S/N] ')).lower()
+    print(f' Criptografado fica: {cript(nome, k1, k2, ran1, ran2)} \nAs chaves são {k1}-{k2}-{ran1}-{ran2}')
+    print('---!!!Lembre-se de salvar essas chaves em algum lugar!!!---')
     keep = str(input('Deseja criptografar mais alguma coisa? [S/N]')).upper().strip()
     if keep == 'S':
         menu()
--- descrip.py
+++ descrip.py
@@ -1,31 +1,31 @@
-alfa = 'abcdefghijklmnopqrstuvwxyz0123456789 '
+alfa = 'nwrx7o2z6vf8kamq1cy4djbu9s0pg3t5ielh '
 
 
-def glis(name, key1, key2):
+def glis(name, key1, key2, rkey1, rkey2):
     secl = []
     for i in range(0, len(name)):
         a = name[i]
 
-        alfa1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        alfa1 = ['n', 'w', 'r', 'x', '7', 'o', '2', 'z', '6', 'v', 'f',
+                 '8', 'k', 'a', 'm', 'q', '1', 'c', 'y', '4', 'd', 'j',
+                 'b', 'u', '9', 's', '0', 'p', 'g', '3', 't', '5', 'i', 'e', 'l', 'h']
         for c in range(0, 36):
             if a == alfa1[c]:
                 a = 5 + c
-                a = (a ** key1 + key2 ** 2) / 230
-                b = (a * 3) - 1 / 4
+                a = (a ** key1 + key2 ** 2) * rkey1 / rkey2
+                b = (a * rkey2) - 1 / (a / 2)
                 a = int(a)
                 b = int(b)
                 b = str(b)
                 a = str(a)
-                a = a + alfa1[c - 3] + b + alfa1[c - 15]
+                a = a + alfa1[c - rkey2] + b + alfa1[c - rkey1]
 
         secl.append(a)
     return secl
 
 
-def descrip(name, k1, k2):
-    mainl = glis(alfa, k1, k2)
+def descrip(name, k1, k2, rkey1, rkey2):
+    mainl = glis(alfa, k1, k2, rkey1, rkey2)
     maind = (gdic(mainl))
 
     for c in range(0, len(mainl)):
@@ -47,11 +47,13 @@ def menu():
     nome = str(input('Digite aqui o que deseja descriptografar: ')).lower().strip()
     k1 = int(input('Digite o valor da primeira chave [0 para padrão]: '))
     k2 = int(input('Digite o valor da segunda chave [0 para padrão]: '))
+    ran1 = int(input('Digite o valor da terceira chave: '))
+    ran2 = int(input('Digite o valor da quarta chave: '))
     if k1 == 0:
         k1 = 2
     if k2 == 0:
         k2 = 6
-    print(f'Descriptografado fica: {descrip(nome, k1, k2)}')
+    print(f'Descriptografado fica: {descrip(nome, k1, k2, ran1, ran2)}')
     keep = str(input('Deseja descriptografar mais alguma coisa? [S/N]')).upper().strip()
     if keep == 'S':
         menu()
